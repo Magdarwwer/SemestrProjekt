0c6215d4aa9c0e78974e199f846cffc91428dc57
Magda <Magda@LAPTOP-K7D4V89L>
2022-06-29 23:21:27 +0200

    Add project files.

---
+++ GardenItHome.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SemestrProjekt
+{
+    /// <summary>
+    /// Interaction logic for GardenItHome.xaml
+    /// </summary>
+    public partial class GardenItHome : Page
+    {
+        public GardenItHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            //GardenReportPage gardenReportPage = new GardenReportPage();
+            //this.NavigationService.Navigate(gardenReportPage);
+
+            // View Atlas Plant Report
+            AtlasPage atlasPage = new AtlasPage();
+            this.NavigationService.Navigate(atlasPage);
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            //GardenReportPage gardenReportPage = new GardenReportPage();
+            //this.NavigationService.Navigate(gardenReportPage);
+
+            // View Expense Report
+            AtlasPage atlasPage = new AtlasPage();
+            this.NavigationService.Navigate(atlasPage);
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            //GardenReportPage gardenReportPage = new GardenReportPage();
+            //this.NavigationService.Navigate(gardenReportPage);
+
+            // View Expense Report
+            GardenReportPage expenseReportPage = new GardenReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            //GardenReportPage gardenReportPage = new GardenReportPage();
+            //this.NavigationService.Navigate(gardenReportPage);
+
+            // View Expense Report
+            GardenReportPage expenseReportPage = new GardenReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+
+
+    }
+}
+
---
+++ SQLQuery4.sql
@@ -0,0 +1,62 @@
+﻿
+CREATE TABLE Atlas
+(
+[nazwa] VARCHAR(20) PRIMARY KEY NOT NULL,
+[stanowisko] NVARCHAR(50) NOT NULL,
+[podlewanie] NVARCHAR(50) NOT NULL,
+[domowa] BIT NOT NULL,	
+[ogrodowa] BIT NOT NULL,
+[nawozenie] VARCHAR(30) NOT NULL,
+[choroby] VARCHAR(100) NOT NULL,
+)
+GO
+
+--Tabela MOJE_ROSLINY
+
+CREATE TABLE Moje_rosliny(
+[imie] VARCHAR(25) NOT NULL PRIMARY KEY,
+[nazwa] VARCHAR(20) NOT NULL UNIQUE,
+[ostatnie_podlewanie] date DEFAULT GetDate() CHECK(ostatnie_podlewanie<= GetDate()) ,
+[ostatnie_nawozenie] date DEFAULT GetDate() CHECK(ostatnie_nawozenie <= GetDate()) ,
+[przebyte_choroby] VARCHAR(50),
+FOREIGN KEY (nazwa) REFERENCES Atlas(nazwa) ON DELETE NO ACTION ON update cascade, 
+)
+GO
+
+--tabela PODLEWANIE
+
+CREATE TABLE Podlewanie(
+[id] INT NOT NULL PRIMARY KEY IDENTITY,
+[imie] VARCHAR(25) NOT NULL,
+[nazwa] VARCHAR(20) NOT NULL,
+[data_pierwszego_podlania] DATE DEFAULT GetDate() CHECK(data_pierwszego_podlania<= GetDate()),
+FOREIGN KEY (nazwa) REFERENCES Atlas(nazwa) ON DELETE NO ACTION ON update NO ACTION,
+FOREIGN KEY (imie) REFERENCES Moje_rosliny(imie) ON DELETE NO ACTION ON update NO ACTION,
+)
+GO
+
+--Tabela nawozenie
+
+CREATE TABLE Nawozenie(
+[id] INT NOT NULL PRIMARY KEY IDENTITY,
+[imie] VARCHAR(25) NOT NULL,
+[nazwa] VARCHAR(20) NOT NULL,
+[data_pierwszego_nawozenia] DATE DEFAULT GetDate() CHECK(data_pierwszego_nawozenia<= GetDate()),
+FOREIGN KEY (nazwa) REFERENCES Atlas(nazwa) ON DELETE NO ACTION ON update NO ACTION,
+FOREIGN KEY (imie) REFERENCES Moje_rosliny(imie) ON DELETE NO ACTION ON update NO ACTION,
+)
+GO
+
+--Tabela uslugi
+
+CREATE TABLE Choroby(
+[id] INT  NOT NULL PRIMARY KEY,
+[nazwa_choroby] VARCHAR(30) NOT NULL,
+[leczenie] VARCHAR(110) NOT NULL,
+[skutki_choroby] VARCHAR(110) NOT NULL,
+[jakich_roslin_dotyczy] VARCHAR(20) NOT NULL,
+FOREIGN KEY (jakich_roslin_dotyczy) REFERENCES Moje_rosliny(nazwa) ON DELETE NO ACTION ON update cascade,
+)
+GO
+
+
